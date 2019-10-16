19ec0a97243ccc193bb49c28825da096f339f9f7
aliya1628 <54244127+aliya1628@users.noreply.github.com>
2019-10-16 16:49:00 +0530

    Initial commit

---
+++ Asp.netCoreMVCCRUD-master/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreMVCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        [Required(ErrorMessage = "This Field is Required.")]
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+        [Column(TypeName = "nvarchar(10)")]
+        [DisplayName("EMP Code")]
+        public string EmpCode { get; set; }
+        [Column(TypeName = "nvarchar(10)")]
+        public string Position { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Office Location")]
+        public string OfficeLocation { get; set; }
+    }
+}
